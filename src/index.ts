089d09bf5166abbf65f197ed72533f5084eb5711
Timm Stelzer <timmstelzer@gmail.com>
2021-03-22 09:49:09 +0100

    feat!: remove Equals since it wasn't part of the published build

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,3 @@ export {
     createConstructor,
 } from './format-error';
 export {parseEnv, parseEnvW} from './parse-env';
-
-/**
- * Utility asserting that _ extends T, representing T.
- *
- * Useful for comparing two types, e.g. the return type of an io-ts codec and
- * some interface.
- *
- * @author https://github.com/gillchristian
- * @since 0.4.10
- *
- * @example simple example
- * type A = string
- * type B = Equals<A, string>
- *
- * @example with io-ts
- * const Codec = io.string;
- * type A = Equals<string, io.TypeOf<typeof Codec>>;
- */
-export type Equals<T, _ extends T> = T;
